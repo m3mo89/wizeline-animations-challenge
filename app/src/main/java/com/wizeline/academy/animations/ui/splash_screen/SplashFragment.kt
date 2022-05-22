946d45359337c2c4e9b39a5bd26539bb56f5b1b0
Guillermo Tinoco <m3mo89@gmail.com>
2022-05-22 00:55:25 -0500

    Update SplashFragment.kt

--- app/src/main/java/com/wizeline/academy/animations/ui/splash_screen/SplashFragment.kt
+++ app/src/main/java/com/wizeline/academy/animations/ui/splash_screen/SplashFragment.kt
@@ -1,5 +1,7 @@
 package com.wizeline.academy.animations.ui.splash_screen
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +9,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.wizeline.academy.animations.R
 import com.wizeline.academy.animations.databinding.SplashFragmentBinding
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -23,6 +25,12 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = SplashFragmentBinding.inflate(inflater, container, false)
+
+        val logoAnimatorSet =
+            AnimatorInflater.loadAnimator(this.context, R.animator.logo_animation) as AnimatorSet
+        logoAnimatorSet.setTarget(binding.ivWizelineLogo)
+        logoAnimatorSet.start()
+
         return binding.root
     }
 
